3131bea7e10d0d1ae4002fd3e71d87dadcd9bb0d
matheuskaio <m.k.m.p.2000@gmail.com>
2020-03-12 21:35:51 -0300

    Primeiro Commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { MdShoppingBasket } from 'react-icons/md';
+import { Container, Cart } from './styles';
+
+import logo from '../../assets/images/logo.svg';
+
+// eslint-disable-next-line react/prop-types
+function Header({ cartSize }) {
+  return (
+    <Container>
+      <Link to="/">
+        <img src={logo} alt="Rocketshoes" />
+      </Link>
+      <Cart to="/cart">
+        <div>
+          <strong>Meu Carrinho</strong>
+          <span>{cartSize} itens</span>
+        </div>
+        <MdShoppingBasket size={36} color="#FFF" />
+      </Cart>
+    </Container>
+  );
+}
+export default connect(state => ({
+  cartSize: state.cart.length,
+}))(Header);
